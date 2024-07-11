cbb2f965e0aeb9dac3f3cf537f2c1d98cc7e8ef8
souleye <sulaynd@gmail.com>
2024-07-11 12:59:36 -0400

    remove logger statement

--- src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -18,7 +18,7 @@ public class SpringJenkinsApplicationTests {
 	@Test
 	public void contextLoads() {
 		logger.info("Test case executing...");
-		logger.info("Test case executing secon log statement");
+		//logger.info("Test case executing secon log statement");
 		assertEquals(true, true);
 	}
 
